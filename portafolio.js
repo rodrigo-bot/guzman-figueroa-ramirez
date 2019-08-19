89db17621aa13cf732e95a1242a8fb50fff4749e
antofigueroa <54033093+antofigueroa@users.noreply.github.com>
2019-08-18 21:25:52 -0400

    Update portafolio.js

--- portafolio.js
+++ portafolio.js
@@ -2,17 +2,17 @@ var trabajos = [{
     figure: "media/RPC19.jpg", 
     alt: "Máquina cosechadora de papas.",
     title: "RCP19",
-    about: "Facilita la carga física del trabajador."
+    about: "Máquina cosechadora de papas."
 }, {
-    figure: "media/trabajo-02-app.jpg",
-    alt: "Descripción de la imagen",
-    title: "Título de segundo trabajo",
-    about: "Texto simulado de descripción de segundo trabajo"
+    figure: "media/Tostador Ilko.jpg",
+    alt: "Campaña para los 50 años del Tostador Ilko.",
+    title: "Tostador Ilko",
+    about: "Campaña para los 50 años del Tostador Ilko."
 }, {
-    figure: "media/trabajo-03-homy.jpg",
+    figure: "media/Plataforma Integra.jpg",
     alt: "Descripción de la imagen",
-    title: "Título de tercer trabajo",
-    about: "Texto simulado de descripción de tercer trabajo"
+    title: "Plataforma web Integra",
+    about: "Facilita a las personas con discapacidad la entrada al mundo laboral "
 }, {
     figure: "media/trabajo-04-lamparadeparafina.jpeg",
     alt: "Luminaria de parafina color azul frío",
