143b106cdcf539321799dded72ea8b1a17cf57f3
Rama2432 <53785254+Rama2432@users.noreply.github.com>
2019-08-19 23:49:55 -0400

    Update portafolio.js

--- portafolio.js
+++ portafolio.js
@@ -3,31 +3,31 @@ var trabajos = [{
     alt: "Herramienta manual cosechadora de papas.",
     title: "RCP19",
     about: "Herramienta manual cosechadora de papas.",
-    url: "https://www.instagram.com/p/Bz-opy-Drgt/"
+    url: "https://www.instagram.com/p/Bz-opy-Drgt/",
 }, {
     figure: "media/Tostador Ilko.jpg",
     alt: "Campaña para los 50 años del Tostador Ilko.",
     title: "Tostador Ilko",
     about: "Campaña para los 50 años del Tostador Ilko.",
-    url: "http://ilkococina.com/producto?cod=1621001&nombre=tostador-estaado"
+    url: "http://ilkococina.com/producto?cod=1621001&nombre=tostador-estaado",
 }, {
     figure: "media/Plataforma Integra.jpg",
     alt: "Facilita a las personas con discapacidad la entrada al mundo laboral",
     title: "Plataforma web de Inclusión",
-    about: "Facilita a las personas con discapacidad la entrada al mundo laboral "
-    url: "https://www.instagram.com/p/Bz-opy-Drgt/"
+    about: "Facilita a las personas con discapacidad la entrada al mundo laboral ",
+    url: "https://www.instagram.com/p/Bz-opy-Drgt/",
 }, {
     figure: "media/trabajo-04-lamparadeparafina.jpeg",
     alt: "Luminaria de parafina color azul frío",
     title: "Lámpara de parafina",
-    about: "Luminaria de parafina color azul frío"
-    url: "https://www.instagram.com/p/Bz-opy-Drgt/"
+    about: "Luminaria de parafina color azul frío",
+    url: "https://www.instagram.com/p/Bz-opy-Drgt/",
 }, {
     figure: "media/trabajo-05-agrocinta.jpeg",
     alt: "Sistema sembrador de semillas controladamente espaciadas",
     title: "Agrocinta",
-    about: "Sistema sembrador de semillas controladamente espaciadas"
-    url: "https://www.instagram.com/p/Bz-opy-Drgt/"
+    about: "Sistema sembrador de semillas controladamente espaciadas",
+    url: "https://www.instagram.com/p/Bz-opy-Drgt/",
 }, {
    figure: "media/Helbert-3.gif",
     alt: "Kit de juego que promueve el desarrollo de habilidades de investigación",
@@ -52,6 +52,6 @@ $.each(trabajos, function(i, val) {
     var uno = '<figure><img src="' + trabajos[i].figure + '" alt="' + trabajos[i].alt + '"/></figure>';
     var dos = '<h4>' + trabajos[i].title + '</h4>';
     var tres = '<p>' + trabajos[i].about + '</p>';
-    var cuatro = '<p><a href="' + trabajos[i].url + '">Más información</a></p>';
+    var cuatro = '<p><a href="' + trabajos[i].url + '">Más</a></p>';
     $('#portafolio').append('<div class="item">' + uno + dos + tres + cuatro +'</div>')
 });
