aa48796a51e5c053394bec00471055dc8dbfa726
Rama2432 <53785254+Rama2432@users.noreply.github.com>
2019-08-24 21:34:20 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-# Portafolia equipo Guzmán, Figueroa y Ramírez
-Tarea procesos de produccion @faco creacion de un portafolio entre 3 integrantes aprendiendo FrontEnd de desarrollo 
-2019 - segundo semestre - Procesos de Producción
+Portafolia equipo Guzmán, Figueroa y Ramírez
+Tarea procesos de produccion @faco creacion de un portafolio entre 3 integrantes aprendiendo FrontEnd de desarrollo 2019 - segundo semestre - Procesos de Producción
 	
 
--- portafolio.js
+++ portafolio.js
@@ -1,48 +1,57 @@
 var trabajos = [{
-    figure: "media/trabajo-01-desfile.jpg",
-    alt: "Descripción de la imagen",
-    title: "Título de primer trabajo",
-    about: "Texto simulado de descripción de primer trabajo"
+    figure: "media/RPC19.jpg", 
+    alt: "Herramienta manual cosechadora de papas.",
+    title: "RCP19",
+    about: "Herramienta manual cosechadora de papas.",
+    url: "https://www.instagram.com/p/B1kDL6bFA1J/",
 }, {
-    figure: "media/trabajo-02-app.jpg",
-    alt: "Descripción de la imagen",
-    title: "Título de segundo trabajo",
-    about: "Texto simulado de descripción de segundo trabajo"
+    figure: "media/Tostador Ilko.jpg",
+    alt: "Campaña para los 50 años del Tostador Ilko.",
+    title: "Tostador Ilko",
+    about: "Campaña para los 50 años del Tostador Ilko.",
+    url: "https://www.instagram.com/p/B1kEdIXF_Ob/",
 }, {
-    figure: "media/trabajo-03-homy.jpg",
-    alt: "Descripción de la imagen",
-    title: "Título de tercer trabajo",
-    about: "Texto simulado de descripción de tercer trabajo"
+    figure: "media/Plataforma Integra.jpg",
+    alt: "Facilita a las personas con discapacidad la entrada al mundo laboral",
+    title: "Plataforma web de Inclusión",
+    about: "Facilita a las personas con discapacidad la entrada al mundo laboral ",
+    url: "https://www.instagram.com/p/B1kD4RIlKIu/",
 }, {
     figure: "media/trabajo-04-lamparadeparafina.jpeg",
     alt: "Luminaria de parafina color azul frío",
-    title: "Lámpara de parafina",
-    about: "Luminaria de parafina color azul frío"
+    title: "Lámpara de parafina sólida",
+    about: "Luminaria de parafina sólida color azul frío",
+    url: "https://www.instagram.com/p/B1kD0ZrF8y4/",
 }, {
     figure: "media/trabajo-05-agrocinta.jpeg",
     alt: "Sistema sembrador de semillas controladamente espaciadas",
     title: "Agrocinta",
-    about: "Sistema sembrador de semillas controladamente espaciadas"
+    about: "Sistema sembrador de semillas controladamente espaciadas",
+    url: "https://www.instagram.com/p/B1j3A7IFqPx/",
 }, {
-    figure: "media/trabajo-06-Bancarlos.gif",
-    alt: "Banca para 3 personas creado por una plancha de 0.3 mm",
-    title: "Bancarlos Sfeir",
-    about: "Texto simulado de descripción de sexto trabajo"
+   figure: "media/Helbert-3.gif",
+    alt: "Kit de juego que promueve el desarrollo de habilidades de investigación",
+    title: "El laboratorio de Helbert",
+    about: "Kit de juego que promueve el desarrollo de habilidades de investigación",
+    url:"https://www.instagram.com/p/B1kDlRWlegh/",
 }, {
-    figure: "media/trabajo-07-materiales.jpg",
-    alt: "Descripción de la imagen",
-    title: "Título de séptimo trabajo",
-    about: "Texto simulado de descripción de séptimo trabajo"
+    figure: "media/Organismos - 4.gif",
+    alt: "Figuras creadas para describir su formas con los ojos vendados",
+    title: "Organismos 3D",
+    about: "Figuras creadas para describir su formas con los ojos vendados",
+    url:"https://www.instagram.com/p/B1kDpFRl5vy/",
 }, {
-    figure: "media/Bancarlos - 3.gif",
-    alt: "Banca para 3 personas creado por una plancha de 0.3 mm",
-    title: "Título de séptimo trabajo",
-    about: "Texto simulado de descripción de séptimo trabajo"
+    figure: "media/Esplat - 5.gif",
+    alt: "Bowl de comida hecho con ilko que logra moler alimentos en el mismo",
+    title: "Esplat",
+    about: "Bowl de comida hecho con ilko que logra moler alimentos en el mismo",
+    url:"https://www.instagram.com/p/B1kDUxolmvT/",
 }];
 // LUEGO CREAMOS PARTES DEL SITIO CON CADA DATO CONTENIDO EN LA VARIABLE
 $.each(trabajos, function(i, val) {
     var uno = '<figure><img src="' + trabajos[i].figure + '" alt="' + trabajos[i].alt + '"/></figure>';
     var dos = '<h4>' + trabajos[i].title + '</h4>';
     var tres = '<p>' + trabajos[i].about + '</p>';
-    $('#portafolio').append('<div class="item">' + uno + dos + tres + '</div>')
+    var cuatro = '<p><a href="' + trabajos[i].url + '">Más</a></p>';
+    $('#portafolio').append('<div class="item">' + uno + dos + tres + cuatro +'</div>')
 });
